3d5cef8d1cf219292442ca5650e9852e6d261b60
Bipul Islam <bipulhstu@gmail.com>
2024-12-21 16:27:19 +0600

    Initial Commit

---
+++ SwiftData-CRUD/SwiftData_CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_CRUDApp.swift
+//  SwiftData-CRUD
+//
+//  Created by Bipul Islam on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
